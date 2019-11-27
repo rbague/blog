1e4648ebe6d3a4d79ad6deaa7387e8c13f618f38
Roger Bagué <rogerbague@gmail.com>
2019-11-27 21:02:06 +0100

    uypdate deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,16 @@
 #!/bin/sh
 
-# If a command fails then the deploy stops
 set -e
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo
 
 # Go To Public folder
 cd public
 
-# Add changes to git.
+# commit and push new changes
 git add .
-
-# Commit changes.
-msg="rebuilding site $(date)"
-if [ -n "$*" ]; then
-	msg="$*"
-fi
-git commit -m "$msg"
-
-# Push source and build repos.
+git commit -m "rebuilding site $(date +'%d/%m/%Y %H:%M:%S')"
 git push origin master
